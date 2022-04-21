ce02b7e7f3bc3a1bada6bde9fd701e25b1c65367
Tom Howland <tomhowland9@gmail.com>
2022-04-21 21:50:39 +0100

    Add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,30 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter&display=swap'
             rel='stylesheet'
           />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+           fetch('/pop-the-kettle-on')
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
