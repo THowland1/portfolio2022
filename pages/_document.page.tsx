de95e7911659aeb7733725da19a7eb4833bd6c45
Tom Howland <tomhowland9@gmail.com>
2022-07-03 17:52:35 +0100

    Add CV section (hidden for now)

---
+++ pages/_document.page.tsx
@@ -0,0 +1,69 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Inter&display=swap'
+            rel='stylesheet'
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+              if(!localStorage.kettlePopped) {
+                fetch('/pop-the-kettle-on').then(_ => localStorage.kettlePopped = true)
+                
+              }
+          `,
+            }}
+          />
+          <script
+            id='darkTheme'
+            dangerouslySetInnerHTML={{
+              __html: `
+          function checkDarkTheme() {
+
+            if (localStorage.theme === 'light') {
+              document.documentElement.classList.remove('dark')
+            } else {
+              document.documentElement.classList.add('dark')
+            }
+            
+            
+          }
+          checkDarkTheme()
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/cv/index.page.tsx
@@ -0,0 +1,53 @@
+import { CloudDownloadIcon, PrinterIcon } from '@heroicons/react/outline';
+
+const cvUrl = 'https://files.tomhowland.com/TOM_HOWLAND_20220702.pdf';
+
+const CvPage = () => {
+  return (
+    <div className='bg-slate-700 absolute inset-0 flex justify-center flex-col gap-8 p-8'>
+      <div className='flex justify-center items-center gap-4'>
+        <a
+          href={cvUrl}
+          download
+          className='justify-center inline-flex whitespace-nowrap text-white hover:underline underline-offset-4'
+        >
+          &larr;&nbsp;Back to home
+        </a>
+        <div className='flex-1'></div>
+        <button
+          onClick={(_) => {
+            try {
+              (
+                document.getElementById('iframe') as HTMLIFrameElement
+              ).contentWindow!.print();
+            } catch (error) {
+              alert('Something went wrong!');
+            }
+          }}
+          className=' pl-4 items-center justify-center inline-flex whitespace-nowrap text-gray-500  bg-gray-200  border-0 py-2 px-6 focus:outline-none hover:bg-gray-300  hover:text-gray-900 rounded text-lg'
+        >
+          <PrinterIcon className='w-4 h-4 my-1 mr-3 inline text-inherit' />
+          Print
+        </button>
+        <a
+          href={cvUrl}
+          download
+          className=' pl-4 items-center justify-center inline-flex whitespace-nowrap text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'
+        >
+          <CloudDownloadIcon className='w-4 h-4 my-1 mr-3 inline text-white' />
+          Download
+        </a>
+      </div>
+      <iframe
+        id='iframe'
+        style={{
+          flex: 1,
+          width: '100%',
+        }}
+        src={cvUrl}
+      ></iframe>
+    </div>
+  );
+};
+
+export default CvPage;
